69e361c34fcc0cff28696521590a80a0b90fd26d
Sogomonyan Hayk <sogomonyanhayk7@gmail.com>
2025-10-01 01:30:14 +0400

    added user schema

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    recipes: [{ type: Schema.Types.ObjectId, ref: "Recipe" }],
+    favorites: [{ type: Schema.Types.ObjectId, ref: "Recipe" }],
+    bio: {
+        type: String,
+        required: true
+    },
+    profilePicture: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model("User", userSchema)
