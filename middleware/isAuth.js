b3758cff1577a73b7d88a2fa0b53302bdaa8f321
Sogomonyan Hayk <sogomonyanhayk7@gmail.com>
2025-10-03 16:54:02 +0400

    now requests have isLoggedIn prop

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -4,6 +4,7 @@ module.exports = (req, res, next) => {
     const authHeader = req.get('Authorization');
 
     if (!authHeader) {
+        req.isLoggedIn = false
         return res.status(401).json({ message: 'Not authenticated. Token missing.' });
     }
 
@@ -13,14 +14,17 @@ module.exports = (req, res, next) => {
     try {
         decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     } catch (err) {
+        req.isLoggedIn = false
         return res.status(401).json({ message: 'Not authenticated. Token invalid.' });
     }
 
     if (!decodedToken) {
+        req.isLoggedIn = false
         return res.status(401).json({ message: 'Not authenticated.' });
     }
 
     // Authentication successful
     req.userId = decodedToken.id;
+    req.isLoggedIn = true;
     next();
 };
